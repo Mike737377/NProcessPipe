d23a4f604cc02d518bc43703eebfa13509a507c9
Mike737377 <no@email.please>
2014-06-27 15:25:56 +1000

    Remove unused dependencies and update dependency graph tests

--- src/NProcessPipe.Tests/DependencyAnalysis/DependencyGraphTests.cs
+++ src/NProcessPipe.Tests/DependencyAnalysis/DependencyGraphTests.cs
@@ -4,11 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Shouldly;
 
 namespace NProcessPipe.Tests.DependencyAnalysis
 {
     public class DependencyGraphTests
     {
+
         [TestFixture]
         public class WhenNoDependencies
         {
@@ -25,37 +27,41 @@ namespace NProcessPipe.Tests.DependencyAnalysis
             [Test]
             public void ListsInAddedOrder()
             {
-                foreach (var o in _graph.Order())
-                {
-                    Console.WriteLine(o);
-                }
+                var order = _graph.Order().ToArray();
+
+                order[0].Data.ShouldBe("c");
+                order[1].Data.ShouldBe("a");
+                order[2].Data.ShouldBe("b");
             }
         }
 
         [TestFixture]
-        public class WithSingleDependency
+        public class WithSingleDependencyChain
         {
             private readonly DependencyGraph<string> _graph;
 
-            public WithSingleDependency()
+            public WithSingleDependencyChain()
             {
                 _graph = new DependencyGraph<string>();
-                _graph.AddNode("3");
-                _graph.AddNode("2");
-                _graph.AddNode("1");
+                _graph.AddNode("3");                
                 _graph.AddNode("0");
+                _graph.AddNode("1");
+                _graph.AddNode("2");
                 _graph.Connect("3", "1");
                 _graph.Connect("3", "2");
+                _graph.Connect("2", "1");
                 _graph.Connect("1", "0");
             }
 
             [Test]
             public void ListsInOrder()
             {
-                foreach (var o in _graph.Order())
-                {
-                    Console.WriteLine(o);
-                }
+                var order = _graph.Order().ToArray();
+
+                order[0].Data.ShouldBe("0");
+                order[1].Data.ShouldBe("1");
+                order[2].Data.ShouldBe("2");
+                order[3].Data.ShouldBe("3");
             }
         }
 
